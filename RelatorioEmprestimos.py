96831a417eccee7b725f3a919908a55a93e7fa71
Lucas Lima <56803833+justAnormlaguy@users.noreply.github.com>
2025-10-23 20:33:18 -0300

    Add files via upload

---
+++ RelatorioEmprestimos.py
@@ -0,0 +1,27 @@
+
+class RelatorioEmprestimos:
+
+    def __init__(self):
+        self.emprestimos = {}
+
+    def adicionar_emprestimo(self, titulo):
+        if titulo in self.emprestimos:
+            self.emprestimos[titulo] += 1
+        else:
+            self.emprestimos[titulo] = 1
+
+    def total_emprestimos(self, titulo):
+        return self.emprestimos.get(titulo, 0)
+
+    def gerar_relatorio(self):
+        if not self.emprestimos:
+            return []
+
+        resultado = []
+        for titulo, count in self.emprestimos.items():
+            resultado.append((titulo, count))
+
+        resultado.sort(key=lambda x: x[1], reverse=True)
+
+        return resultado
+
